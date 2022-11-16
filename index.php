289eb64bd763684aa389d1b4b0ee56215b816606
Pablo Machado <pmmachadov@gmail.com>
2022-11-16 19:50:32 +0100

    PHP anexo2-pag22-ej1 done.

---
+++ index.php
@@ -0,0 +1,7 @@
+<!--1.- Crear una página que muestre el mensaje “HOLA MUNDO” repetidos diez veces.
+Hazlo empleando código en línea.-->
+
+<?php
+for ($i=1; $i<=10; $i++) {
+    echo "Hola Mundo"."</br>"; 
+}
